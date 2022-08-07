c5b36319d2c80ea23d71177052821c6e24cb0cfc
lsk <3075929352@qq.com>
2022-08-07 22:44:45 +0800

    Project Creation and Main Activity

---
+++ app/src/main/java/com/lsk/bigimageviewer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.lsk.bigimageviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText urlInput;
+    private Button downloadButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.urlInput = findViewById(R.id)
+
+    }
+}
