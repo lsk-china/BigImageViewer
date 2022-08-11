3877c1685d49874e6e16adf0e2e63e6d9eed0867
lsk <3075929352@qq.com>
2022-08-11 22:36:22 +0800

    Finish logic for downloading

--- app/src/main/java/com/lsk/bigimageviewer/AskForNameDialog.java
+++ app/src/main/java/com/lsk/bigimageviewer/AskForNameDialog.java
@@ -2,13 +2,70 @@ package com.lsk.bigimageviewer;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.function.Consumer;
 
 public class AskForNameDialog extends Dialog {
 
-    private String name;
+    private Button okButton;
+    private Button cancelButton;
+    private EditText text;
+    private Consumer<String> onOk;
+    private Consumer<String> onCancel;
 
-    public AskForNameDialog(Context context, String name) {
+    public AskForNameDialog(Context context) {
         super(context);
-        this.name = name;
+    }
+
+    public void setOnOk(Consumer<String> onOk) {
+        this.onOk = onOk;
+    }
+
+    public void setOnCancel(Consumer<String> onCancel) {
+        this.onCancel = onCancel;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ask_for_name_dialog);
+        setCanceledOnTouchOutside(false);
+
+        this.okButton = findViewById(R.id.asK_for_name_ok);
+        this.cancelButton = findViewById(R.id.asK_for_name_cancel);
+        this.text = findViewById(R.id.urlInput);
+
+        this.okButton.setOnClickListener(view -> {
+            if (this.onOk == null) {
+                return;
+            }
+            this.onOk.accept(this.text.getText().toString());
+        });
+
+        this.cancelButton.setOnClickListener(view -> {
+            if (this.onCancel == null) {
+                return;
+            }
+            this.onCancel.accept(this.text.getText().toString());
+        });
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+        layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+        getWindow().getDecorView().setPadding(0, 0, 0, 0);
+        getWindow().setAttributes(layoutParams);
     }
 }
--- app/src/main/java/com/lsk/bigimageviewer/MainActivity.java
+++ app/src/main/java/com/lsk/bigimageviewer/MainActivity.java
@@ -1,29 +1,49 @@
 package com.lsk.bigimageviewer;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.service.controls.templates.RangeTemplate;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.downloader.Error;
+import com.downloader.OnDownloadListener;
 import com.downloader.PRDownloader;
 import com.downloader.PRDownloaderConfig;
+import com.downloader.Progress;
+import com.downloader.request.DownloadRequestBuilder;
 import com.wanjian.cockroach.Cockroach;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText urlInput;
     private Button downloadButton;
-    private int jobId;
     private File dataDir;
+    private ProgressDialog downloadProgress;
+    private Handler progressHandler;
 
+    private static final int PROGRESS_DIALOG_SHOW = 0;
+    private static final int PROGRESS_DIALOG_DISMISS = 1;
+    private static final int PROGRESS_DIALOG_PROGRESS = 2;
+    private static final int PROGRESS_DIALOG_FAILED = 3;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +52,34 @@ public class MainActivity extends AppCompatActivity {
         Cockroach.install((thread, throwable) -> {
             Toast.makeText(getApplicationContext(), "Thread: " + thread.getName() + " threw an uncaught exception: " + throwable.getMessage(), Toast.LENGTH_SHORT);
             Log.e("BigImageViewer:"+thread.getName(), "Caught exception: " + throwable.getClass().getCanonicalName() + ": " + throwable.getMessage(), throwable);
+        });
+
+        this.downloadProgress = new ProgressDialog(this);
+        this.downloadProgress.setTitle("Downloading...");
+        this.downloadProgress.setMax(100);
+        this.downloadProgress.setProgress(0);
+        this.downloadProgress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        this.progressHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(@NonNull Message message) {
+                switch (message.what) {
+                    case PROGRESS_DIALOG_SHOW:
+                        downloadProgress.setTitle((String) message.obj);
+                        downloadProgress.show();
+                        break;
+                    case PROGRESS_DIALOG_DISMISS:
+                        downloadProgress.dismiss();
+                        break;
+                    case PROGRESS_DIALOG_PROGRESS:
+                        downloadProgress.setProgress(message.arg1);
+                        break;
+                    case PROGRESS_DIALOG_FAILED:
+                        downloadProgress.dismiss();
+                        Toast.makeText(getApplicationContext(), (String) message.obj, Toast.LENGTH_SHORT);
+                        break;
+                }
+                return true;
+            }
         });
 
         try {
@@ -63,22 +111,80 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
 
-            try {
-                PRDownloader.download(url, dataDir.getCanonicalPath(), parseFilename(url));
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
+            parseFilename(url, s -> {
+                if (s.equals("")) {
+                    return;
+                } else {
+                    try {
+                        DownloadRequestBuilder download = PRDownloader.download(url, dataDir.getCanonicalPath(), s);
+                        download.build()
+                                .setOnStartOrResumeListener(() -> {
+                                    Message message = Message.obtain(progressHandler);
+                                    message.obj = s;
+                                    message.what = PROGRESS_DIALOG_SHOW;
+                                    message.sendToTarget();
+                                })
+                                .setOnProgressListener(progress -> {
+                                    int realProg = (int) Math.floor(progress.currentBytes / progress.totalBytes);
+                                    Message message = Message.obtain(progressHandler);
+                                    message.arg1 = realProg;
+                                    message.what = PROGRESS_DIALOG_PROGRESS;
+                                    message.sendToTarget();
+                                })
+                                .start(new OnDownloadListener() {
+                                    @Override
+                                    public void onDownloadComplete() {
+                                        try {
+                                            Message message = Message.obtain(progressHandler);
+                                            message.what = PROGRESS_DIALOG_DISMISS;
+                                            message.sendToTarget();
+                                            Bundle bundle = new Bundle();
+                                            bundle.putString("filename", s);
+                                            bundle.putString("storagePath", dataDir.getCanonicalPath());
+                                            Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                                            intent.putExtras(bundle);
+                                            startActivity(intent);
+                                        } catch (Exception e) {
+                                            throw new RuntimeException(e);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onError(Error error) {
+                                        Message message = Message.obtain(progressHandler);
+                                        message.what = PROGRESS_DIALOG_FAILED;
+                                        message.obj = error.getResponseCode() + ": " + error.getServerErrorMessage();
+                                        message.sendToTarget();
+                                    }
+                                });
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
         });
 
     }
 
-    private String parseFilename(String url) {
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void parseFilename(String url, Consumer<String> filename) {
         String[] parts = url.split("/"); // https://xxxx.xxx:xxx/a/b/c.jpg -> https:, xxx.xxx:xxx, a, b, c.jpg
         String lastPart = parts[parts.length - 1]; // c.jpg
         if (lastPart.indexOf(".") == -1) {
             // User didn't provide a file name, then ask for it.
-
+            final AskForNameDialog askForNameDialog = new AskForNameDialog(getApplicationContext());
+            askForNameDialog.setOnCancel(s -> {
+                askForNameDialog.dismiss();
+                filename.accept(s);
+            });
+            askForNameDialog.setOnCancel(s -> {
+                askForNameDialog.dismiss();
+                Toast.makeText(getApplicationContext(), "A filename should be provided!", Toast.LENGTH_SHORT);
+                filename.accept("");
+            });
+            askForNameDialog.show();
+        } else {
+            filename.accept(lastPart);
         }
-        return "";
     }
 }
