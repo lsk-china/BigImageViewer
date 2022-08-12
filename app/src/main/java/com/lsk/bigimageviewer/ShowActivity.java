c60ffaed1c7ba92a7c45387fcb164cc4101af01a
lsk <3075929352@qq.com>
2022-08-12 08:32:00 +0800

    show image

--- app/src/main/java/com/lsk/bigimageviewer/ShowActivity.java
+++ app/src/main/java/com/lsk/bigimageviewer/ShowActivity.java
@@ -2,13 +2,28 @@ package com.lsk.bigimageviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
 public class ShowActivity extends AppCompatActivity {
+
+    private SubsamplingScaleImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
+        this.imageView = findViewById(R.id.imageView);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String filename = bundle.getString("filename");
+        String storagePath = bundle.getString("storagePath");
+        Log.i("BigImageViewer:show", filename+storagePath);
+        imageView.setImage(ImageSource.uri(filename + storagePath));
     }
 }
